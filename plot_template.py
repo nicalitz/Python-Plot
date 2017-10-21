d5cc488eb3ec29e8f49c170c1b36d82279cc9811
Nico Calitz <nicalitz@hotmail.com>
2017-10-21 17:17:41 +0200

    Added image import to template

--- plot_template.py
+++ plot_template.py
@@ -1,11 +1,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import matplotlib as mpl
 mpl.use('Qt5Agg') # specify backend
 
 plt.style.use(os.path.join('Stylesheets', 'matlab_stylesheet.mplstyle'))
-
-data = np.random.randn(50)
-plt.plot(data)
-plt.show()
